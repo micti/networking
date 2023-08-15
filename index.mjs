58ad9df861652b43d3312a59fe58cb274251d491
micti <lenhatanh86@gmail.com>
2023-08-15 08:19:07 +0700

    Update test

--- index.mjs
+++ index.mjs
@@ -1,7 +1,8 @@
 import request from './src/http/request.mjs'
 
 // const requestTo = 'https://tools.scrapfly.io/api/fp/ja3?extended=1'
-const requestTo = 'https://api.publicapis.org/entries'
+// const requestTo = 'https://api.publicapis.org/entries'
+const requestTo = 'https://127.0.0.1:8000/a/s/d/d/d'
 
 ;(async () => {
   const t1 = Date.now()
@@ -10,7 +11,7 @@ const requestTo = 'https://api.publicapis.org/entries'
   const t2 = Date.now()
   console.log('>> finish', t2)
   console.log('>> In', t2 - t1)
-  console.log('>>>', a.time)
-  // console.log(a.response[0])
-  a.response.forEach(e => console.log(e));
+  console.log(a)
+  a.response.forEach(e => console.log(e))
+  a.time.forEach(e => console.log(e))
 })()
--- tiny/request.mjs
+++ tiny/request.mjs
@@ -3,11 +3,16 @@ import https from 'https';
 console.log('s', Date.now())
 https.get('https://api.publicapis.org/entries', (res) => {
   const data = []
+  const time = []
   console.log('>', Date.now())
-  res.on('data', d => data.push(d))
+  res.on('data', d => {
+    data.push(d)
+    time.push(Date.now())
+  })
   res.on('end', () => {
     // Buffer.concat(data).toString('utf-8')
     console.log('>', Date.now())
-    data.forEach(e => console.log(e));
+    data.forEach(e => console.log(e))
+    time.forEach(t => console.log(t))
   })
 })
